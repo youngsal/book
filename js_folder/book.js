91f5462d974218de8b5aef33b69e45cddeed8324
youngsal <waleola805@gmail.com>
2024-11-18 09:41:05 +0100

    my bookmark project

---
+++ js_folder/book.js
@@ -0,0 +1,12 @@
+        // // Function to update the displayed width
+        // function updateWidth() {
+        //     const viewportWidth = window.innerWidth; // Get the current viewport width
+        //     document.getElementById('width-display').innerText = 
+        //         `Viewport width: ${viewportWidth}px`;
+        // }
+
+        // // Run the updateWidth function initially
+        // updateWidth();
+
+        // // Add an event listener to update the width whenever the window is resized
+        // window.addEventListener('resize', updateWidth);
