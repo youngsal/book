40d466975b055544bcd8766c9554b9ebddc41fdd
youngsal <waleola805@gmail.com>
2024-11-20 13:48:24 +0100

    update

--- javascript_folder/book.js
+++ javascript_folder/book.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+    // ======features easy
+    $(".easy_btn").click(function(){
+        $(".easy_btn").not($(this)).removeClass("easy_active");
+        $(this).toggleClass("easy_active");
+    });
+
+    // ========easy tabs
+    $(".easy_btn").click(function(){
+        const tabId = $(this).data("target"); // get the data value
+
+        $(".easy_tab").removeClass("active_tab").hide();
+        $(`#${tabId}`).addClass("active_tab").show();
+    });
+
+    // ========downloader_Section
     $(".normal_article_div").click(function(){
         // slideUp all article except from the recent one that was clicked
         $(".question_article").not($(this).next(".question_article")).slideUp();
@@ -7,10 +22,5 @@ $(document).ready(function(){
         // toggle slide up and down if any of the article is been clicked
         $(this).next(".question_article").slideToggle();
         $(this).find("ion-icon").toggleClass("rotate");
-    });
-
-    $(".easy_btn").click(function(){
-        $(".easy_btn").not($(this)).removeClass("easy_active");
-        $(this).toggleClass("easy_active");
     });
 ;});
