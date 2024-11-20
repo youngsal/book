a9fd7128044e77e52d109a7d016a55084a71e8a9
youngsal <waleola805@gmail.com>
2024-11-20 10:19:08 +0100

    update on jquery

--- javascript_folder/book.js
+++ javascript_folder/book.js
@@ -1,3 +1,16 @@
-// $(document).ready(function(){
-//     console.log("it's working");
-// });
+$(document).ready(function(){
+    $(".normal_article_div").click(function(){
+        // slideUp all article except from the recent one that was clicked
+        $(".question_article").not($(this).next(".question_article")).slideUp();
+        $(".arrow_div ion-icon").not($(this).find("ion-icon")).removeClass("rotate");
+
+        // toggle slide up and down if any of the article is been clicked
+        $(this).next(".question_article").slideToggle();
+        $(this).find("ion-icon").toggleClass("rotate");
+    });
+
+    $(".easy_btn").click(function(){
+        $(".easy_btn").not($(this)).removeClass("easy_active");
+        $(this).toggleClass("easy_active");
+    });
+;});
